43a20103d4433a53c90c8f3b00b72f9de51016b4
rummolprod999 <rummolprod999@gmail.com>
2019-11-14 14:45:21 +0300

    add timeouts

--- run_parser.py
+++ run_parser.py
@@ -3,12 +3,13 @@ import urllib.parse
 import urllib.request
 
 import lxml.html
+import timeout_decorator
 
 export_text = 'customers.txt'
 
 
 def main():
-    for i in range(1, 798):
+    for i in range(17, 798):
         url = f"https://agregatoreat.ru/clients/customers/addresses=москва&status=1/page/{i}"
         try:
             print(i)
@@ -22,10 +23,16 @@ def get_page(url):
     url = list(url)
     url[2] = urllib.parse.quote(url[2])
     url = urllib.parse.urlunsplit(url)
-    req = urllib.request.Request(url=url)
-    handler = urllib.request.urlopen(req)
-    page = handler.read().decode('utf-8')
+    page = download_page(url)
     extract_num_customer(page)
+
+
+@timeout_decorator.timeout(35)
+def download_page(url):
+    req = urllib.request.Request(url=url)
+    handler = urllib.request.urlopen(req, timeout=30)
+    page = handler.read().decode('utf-8')
+    return page
 
 
 def extract_num_customer(page):
@@ -39,9 +46,7 @@ def extract_num_customer(page):
 
 def page_customer_download(num):
     url = f"https://agregatoreat.ru/organization/{num}"
-    req = urllib.request.Request(url=url)
-    handler = urllib.request.urlopen(req)
-    page = handler.read().decode('utf-8')
+    page = download_page(url)
     extract_from_customer(page)
 
 
@@ -49,7 +54,7 @@ def extract_from_customer(page):
     doc = lxml.html.document_fromstring(page)
     name = doc.xpath('.//div[. = "Полное наименование"]/following-sibling::div')[0].text_content()
     email = doc.xpath('.//div[. = "Адрес электронной почты"]/following-sibling::div/span')[0].text_content().strip(
-        ' \t\n')
+            ' \t\n')
     phone = ''
     try:
         phone = doc.xpath('.//div[. = "Телефон"]/following-sibling::div/span')[0].text_content().strip(
